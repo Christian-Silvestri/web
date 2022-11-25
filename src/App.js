f73cd497e66d37521a01bbfa86954442a06e1462
Christian Silvestri <christiansilvestri@live.it>
2022-11-25 18:22:26 +0100

    loader animation test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import LogoCS from "./component/LogoCS";
 import Main from "./components/Main";
 import Skills from "./component/Skills";
@@ -9,6 +9,9 @@ import Footer from './components/Footer';
 import Carrer from "./components/Carrer";
 import About from "./component/About";
 import Contact from "./component/Contact";
+import { AnimatePresence, motion } from "framer-motion";
+import Loader from "./component/Loader";
+
 
 
 const { Layout } = Section();
@@ -17,13 +20,68 @@ const { Layout } = Section();
 
 function App() {
 
+  const [isLoading, setIsLoading] = useState(true);
+
+   useEffect(() => {
+     setIsLoading(true);
+     setTimeout(() => {
+       setIsLoading(false)
+     }, 5000)
+   }, [])
+
+  const animation = {
+    hidden: {
+      opacity: [0, 0.2, 0.5],
+      scaleY: 0,
+
+      
+    },
+    visible: {
+      opacity: 1,
+      scaleY: 1,
+
+      transition: {
+        duration: 0.5,
+        delay: 1
+
+      }
+    },
+    exit: {
+      opacity: 0,
+      scaleY: 0,
+      transition: {
+        duration: 0.5
+      }
+    }
+  }
+
 
   return (
     <>
       <div className="bg-zinc-800 text-white ">
-        <About />
-        <Skills />
-        <Contact />
+        <AnimatePresence>
+          {isLoading && (
+
+            <div className="flex items-end  justify-center h-screen">
+              <Loader />
+              <motion.h1 initial={'hidden'} animate={'visible'} variants={animation} exit={'exit'} key={1} className="text-white flex flex-col pb-24 xl:pb-12">
+                <span>
+                  hi, i'm
+                </span>
+                christian silvestri
+              </motion.h1>
+            </div>
+
+          )}
+        </AnimatePresence>
+        <>
+          <About />
+          <Skills />
+          <Contact />
+        </>
+
+
+
       </div>
     </>
 
